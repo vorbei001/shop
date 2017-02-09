14fa4be6440372737725e4a6189533244e23ad3d
YangFan0304 <15010958171@163.com>
2017-02-09 15:18:51 +0800

    底部切换导航

--- app/src/main/java/text/bw/com/shop/MainActivity.java
+++ app/src/main/java/text/bw/com/shop/MainActivity.java
@@ -1,5 +1,6 @@
 package text.bw.com.shop;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -13,6 +14,7 @@ import text.bw.com.shop.Adapter.MyAdapter;
 import text.bw.com.shop.Fragment.Fragment1;
 import text.bw.com.shop.Fragment.Fragment2;
 import text.bw.com.shop.Fragment.Fragment3;
+import text.bw.com.shop.Fragment.Fragment4;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +40,46 @@ public class MainActivity extends AppCompatActivity {
         Fragment1 f1 = new Fragment1();
         Fragment2 f2 = new Fragment2();
         Fragment3 f3 = new Fragment3();
+        Fragment4 f4 = new Fragment4();
+        list.add(f1);
+        list.add(f2);
+        list.add(f3);
+        list.add(f4);
         vp.setAdapter(new MyAdapter(getSupportFragmentManager(),this,list));
+        vp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < rg.getChildCount(); i++) {
+                    RadioButton rb = (RadioButton) rg.getChildAt(i);
+                    if (position==i) {
+                        rb.setChecked(true);
+                        rb.setTextColor(Color.RED);
+                    }else {
+                        rb.setChecked(false);
+                        rb.setTextColor(Color.parseColor("#999999"));
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                for (int i = 0; i < rg.getChildCount(); i++) {
+                    if (rg.getChildAt(i).getId()==checkedId){
+                        vp.setCurrentItem(i);
+                    }
+                }
+            }
+        });
     }
 }
